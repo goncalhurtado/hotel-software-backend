e817022357242db931f462f8e5e293c5291ef880
goncalhurtado <gonzalomilano@gmail.com>
2023-11-14 18:54:38 -0300

    added the basic create booking funct

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const { getAllBookings } = require('../controllers/bookingControllers');
+const { getAllBookings, createBooking } = require('../controllers/bookingControllers');
 
 const router = require('express').Router();
 
 
 router.get("/bookings", getAllBookings);
+router.post("/bookings", createBooking);
 
 module.exports = router;
